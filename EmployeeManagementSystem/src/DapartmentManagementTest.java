7fdf3b708658b58fecf1f47f4bcee66d32d8388d
Ephiphania Munava <ephiphania.munava@standardbank.co.za>
2017-04-11 11:00:32 +0200

    Add files via upload
    Employee Management System Project

---
+++ EmployeeManagementSystem/src/DapartmentManagementTest.java
@@ -0,0 +1,17 @@
+import junit.framework.TestCase;
+
+public class DapartmentManagementTest extends TestCase {
+	
+	public static DepartmentManagement dm = new DepartmentManagement();
+	
+	@test
+	public void Test(){
+		
+		//Create instance of DepartmentManagement
+		
+		
+		assertEquals(, dm.getName());		
+	}
+
+}
+
---
+++ EmployeeManagementSystem/src/EmployeeManagementTest.java
@@ -0,0 +1,5 @@
+import junit.framework.TestCase;
+
+public class EmployeeManagementTest extends TestCase {
+
+}
